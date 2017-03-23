af6f80628033f22860736db35aeb52ae435238ee
DimitriosHytiroglou <dimitrios.hytiroglou@be.pwc.com>
2017-03-23 17:26:10 +0100

    n

--- server/start_cpsv-ap_validator-server.sh
+++ server/start_cpsv-ap_validator-server.sh
@@ -1,1 +1,1 @@
-node server.js 3001 sessions.json 1 52.50.205.146 80 sparql-graph-crud cpsv-ap.semic.eu > server.log
+node server.js 3001 sessions.json 1 52.50.205.146 80 sparql-graph-crud http://cpsv-ap.semic.eu > server.log
